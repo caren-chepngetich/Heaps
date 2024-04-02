02ecc2b09e93b80854af0143141b56c1302e1c2b
Student <Caren-Chepngetich>
2024-04-02 17:18:06 +0300

    made changes

--- index.py
+++ index.py
@@ -1,4 +1,17 @@
-
+# creating a Heap
+import heapq
 values = [6,8,1,9,7,2]
 heapq.heapify(values)
-print
+print(values)
+
+# Inserting a Heap
+heapq.heappush(values,21)
+print(values)
+
+# Removing from heap
+heapq.heappop(values)
+print(values)
+
+# Replacing in a Heap
+heapq.heapreplace(values,45)
+print(values)
